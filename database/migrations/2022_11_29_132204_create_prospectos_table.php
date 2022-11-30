0880002008d1b447531d78c160174991ecfe13bf
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-11-30 08:51:52 -0400

    marketing

---
+++ database/migrations/2022_11_29_132204_create_prospectos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prospectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->string('interes');
+            $table->text('observaciones');
+            $table->string('estado')->nullable();
+            $table->string('carrera')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prospectos');
+    }
+};
